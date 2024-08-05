26317f4b758f056ccdf5c2bf96ddb1df94902a92
Merari Jazel <jazelmerari@gmail.com>
2024-08-04 18:12:15 -0700

    Subiendo proyecto

---
+++ app/src/main/java/com/firebase/firebasekotlin/objetos/ReferenciasFirebase.kt
@@ -0,0 +1,8 @@
+package com.firebase.firebasekotlin.objetos
+
+object ReferenciasFirebase {
+    const val URL_DATABASE = "https://kotlin-353fa-default-rtdb.firebaseio.com/"
+    const val DATABASE_NAME = "agenda"
+    const val TABLE_NAME = "contactos"
+}
+
